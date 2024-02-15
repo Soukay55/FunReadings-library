cf5d8a0175749bcb859ee221c342a0c8011334e1
wisoums <wissem_oumsalem@yahoo.ca>
2024-02-15 14:03:38 -0500

    rien

--- Driver.java
+++ Driver.java
@@ -1,11 +1,6 @@
 public class Driver {
 
     public static void main(String[] args) {
-        System.out.println("hello");
 
-        System.out.println("Souk est nulle");
-
-        System.out.println("cacapipi");
-        System.out.println("salut");
     }
 }
