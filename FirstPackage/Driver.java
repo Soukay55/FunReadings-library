0e457d03ac7fc02a9e03ac92c688d347d9594017
wisoums <wissem_oumsalem@yahoo.ca>
2024-02-15 22:53:30 -0500

    J'ai ajouté les commentaires des methods a faire dans le main

--- FirstPackage/Driver.java
+++ FirstPackage/Driver.java
@@ -2,7 +2,37 @@ package FirstPackage;
 
 public class Driver {
 
-    public static void main(String[] args) {
+    //Method Add Item addItem() :
 
+    //Method Delete Item deleteItem() :
+
+    //Method to change information of an Item changeInformationItem() :
+
+    //Method to list all the books listAllBooks() :
+
+    //Method to list all the journals listAllJournal() :
+
+    //Method to list all the medias listAllMedia() :
+
+    //Method to add a client addClient() :
+
+    //Method to edit a client editClient() :
+
+    //Method to delete a client deleteClient() :
+
+    //Method to lease an item to client leaseItemToClient() :
+
+    //Method to return an item of a client returnItemFromClient() :
+
+    //Method to display the leased items of a client displayLeasedItemOfClient() :
+
+    //Method to display the returned items of all the clients displayReturnedItemsOfAllClients() :
+
+    //Display the biggest book getBiggestBook() :
+
+    //Copy books copyBooks() :
+
+    public static void main(String[] args) {
     }
+
 }
